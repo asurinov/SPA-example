a527edbe78c1ec9e8f91300195bddfb0194d5d8c
Суринов Александр <asurinov8@gmail.com>
2015-06-14 13:08:32 +0300

    Designer

---
+++ Framework/app/designer/designerController.js
@@ -0,0 +1,34 @@
+﻿angular.module("designer").controller("designerController", ["$scope", function ($scope) {
+    $scope.fields = [];
+    $scope.item = {
+        name: "",
+        type: 2
+    }
+    $scope.types = [
+        {
+            id: 1,
+            name: "One"
+        },
+        {
+            id: 2,
+            name: "Two"
+        },
+        {
+            id: 3,
+            name: "Three"
+        }
+    ];
+
+
+    $scope.add = function () {
+        $scope.fields.push($scope.item);
+        clearForm();
+    };
+
+    function clearForm() {
+        $scope.item = {
+            name: "",
+            type: 1
+        }
+    }
+}]);
